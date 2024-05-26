ca566f4b3d090d26b5e32dc7b03deac0ac33fb14
sameem.z@dplit.com-at-401940162791 <sameem.z@dplit.com>
2024-05-26 23:21:09 +0500

    font ,svg, libraries added

--- App.js
+++ App.js
@@ -1,14 +1,47 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import Theme from './src/theme';
+import { widthPercentageToDP } from 'react-native-responsive-screen';
 const App = () => {
   return (
-    <View>
-      <Text>App</Text>
+    <View style={styles.container}>
+      <Text style={styles.quicksandRegular}>
+        This text uses a quick sand font
+      </Text>
+      <Text style={styles.quicksandLight}>
+        This text uses a quick sand light font
+      </Text>
+      <Text style={styles.ralewayThin}>
+        This text uses a thin italic raleway font
+      </Text>
+      <Text style={styles.ralewayItalic}>
+        This text uses a thin italic raleway font
+      </Text>
     </View>
-  )
-}
-
-export default App
-
-const styles = StyleSheet.create({})
+  );
+};
+export default App;
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'lavender',
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  quicksandLight: {
+    fontFamily: Theme.fonts.FONT_NUNITO_REGULAR,
+    fontSize: widthPercentageToDP(5),
+  },
+  quicksandRegular: {
+    fontFamily: Theme.fonts.FONT_NUNITO_BOLD,
+    fontSize: widthPercentageToDP(5),
+  },
+  ralewayItalic: {
+    fontFamily: Theme.fonts.FONT_NUNITO_EXTRABOLD,
+    fontSize: widthPercentageToDP(5),
+  },
+  ralewayThin: {
+    fontFamily: Theme.fonts.FONT_NUNITO_EXTRABOLD_ITALIC,
+    fontSize: widthPercentageToDP(5),
+  },
+});
---
+++ src/theme/index.js
@@ -0,0 +1,15 @@
+const Theme = {
+    fonts: {
+        FONT_NUNITO_BOLD: 'Nunito-Bold',
+        FONT_NUNITO_MEDIUM: 'Nunito-Medium',
+        FONT_NUNITO_REGULAR: 'Nunito-Regular',
+        FONT_NUNITO_SEMIBOLD: 'Nunito-SemiBold',
+        FONT_NUNITO_EXTRABOLD: 'Nunito-ExtraBold',
+        FONT_NUNITO_BOLD_ITALIC: 'Nunito-BoldItalic',
+        FONT_NUNITO_MEDIUM_ITALIC: 'Nunito-MediumItalic',
+        FONT_NUNITO_REGULAR_ITALIC: 'Nunito-Italic',
+        FONT_NUNITO_SEMIBOLD_ITALIC: 'Nunito-SemiBoldItalic',
+        FONT_NUNITO_EXTRABOLD_ITALIC: 'Nunito-ExtraBoldItalic',
+    }
+};
+export default Theme;
